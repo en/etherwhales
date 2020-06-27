47112e096c8b4ab2f11fd80041d9f6a5f10f336e
Yuanchao Sun <yuanchao.sun@gmail.com>
2020-06-28 07:36:33 +0800

    Watching token transfer

--- src/eth.rs
+++ src/eth.rs
@@ -1,21 +1,89 @@
-use web3::futures::{future, StreamExt};
+use serde_json::json;
+use tiny_keccak::{Hasher, Keccak};
+use warp::ws::Message;
+use web3::futures::StreamExt;
+use web3::types::{Address, FilterBuilder, H160, H256, U256};
 
-pub async fn subscribe(endpoint: &str) -> web3::Result {
-    let ws = web3::transports::WebSocket::new(endpoint).await?;
+use crate::Users;
+
+/// Compute the Keccak-256 hash of input bytes.
+fn keccak256(bytes: &[u8]) -> [u8; 32] {
+    let mut output = [0u8; 32];
+    let mut hasher = Keccak::v256();
+    hasher.update(bytes);
+    hasher.finalize(&mut output);
+    output
+}
+
+pub async fn subscribe(endpoint: String, users: Users) -> web3::Result {
+    let ws = web3::transports::WebSocket::new(&endpoint).await?;
     let web3 = web3::Web3::new(ws.clone());
-    let mut sub = web3.eth_subscribe().subscribe_new_heads().await?;
 
+    let bat: Address = "0x0D8775F648430679A709E98d2b0Cb6250d2887EF"
+        .trim_start_matches("0x")
+        .parse()
+        .unwrap();
+    let comp: Address = "0xc00e94Cb662C3520282E6f5717214004A7f26888"
+        .trim_start_matches("0x")
+        .parse()
+        .unwrap();
+    let lend: Address = "0x80fB784B7eD66730e8b1DBd9820aFD29931aab03"
+        .trim_start_matches("0x")
+        .parse()
+        .unwrap();
+
+    let log_transfer_sig = b"Transfer(address,address,uint256)";
+    let log_transfer_sig_hash = keccak256(&log_transfer_sig[..]);
+
+    let filter = FilterBuilder::default()
+        .address(vec![bat, comp, lend])
+        .topics(
+            Some(vec![H256::from_slice(&log_transfer_sig_hash)]),
+            None,
+            None,
+            None,
+        )
+        .build();
+
+    let sub = web3.eth_subscribe().subscribe_logs(filter).await?;
     println!("Got subscription id: {:?}", sub.id());
 
-    (&mut sub)
-        .take(5)
-        .for_each(|x| {
-            println!("Got: {:?}", x);
-            future::ready(())
-        })
-        .await;
+    // Ok(Log { address: 0xc00e94cb662c3520282e6f5717214004a7f26888, topics: [0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef, 0x0000000000000000000000003f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be, 0x00000000000000000000000084e1212fbd2af43ae525ec760830fecf4a06d046], data: Bytes([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 221, 251, 201, 115, 161, 61, 0, 0]), block_hash: Some(0xee7dc738e07ad53c0eb2b1b7d8d4eee3e87da1554d0adc761738153ec77e63e0), block_number: Some(10349643), transaction_hash: Some(0x9586d8c150930012203d5ebd69218014bc16f22d08943af89e05211aa1de3513), transaction_index: Some(8), log_index: Some(7), transaction_log_index: None, log_type: None, removed: Some(false) })
+    sub.for_each(|log| async {
+        let log = log.unwrap();
+        // timestamp
+        let symbol = if log.address == bat {
+            "BAT"
+        } else if log.address == comp {
+            "COMP"
+        } else if log.address == lend {
+            "LEND"
+        } else {
+            "UNKNOWN"
+        };
 
-    sub.unsubscribe();
+        let amount: U256 = log.data.0[..32].into();
+
+        let amount = amount / U256::exp10(18);
+        let amount = amount.low_u64(); // TODO: serde_json bug
+
+        let from: H160 = log.topics[1].into();
+        let to: H160 = log.topics[2].into();
+        let message = json!({
+            "timestamp": 0,
+            "symbol": symbol,
+            "amount": amount,
+            "from": from,
+            "to": to,
+        });
+        println!("{}", message);
+        for (_, tx) in users.read().await.iter() {
+            if let Err(disconnected) = tx.send(Ok(Message::text(message.to_string()))) {
+                println!("{}", disconnected);
+            }
+        }
+    })
+    .await;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use std::collections::HashMap;
 use std::env;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
 
 use futures::{FutureExt, StreamExt};
-use warp::Filter;
+use tokio::sync::{mpsc, RwLock};
+use warp::{
+    ws::{Message, WebSocket},
+    Filter,
+};
 
 mod eth;
+
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
+type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
     let infura_endpoint = env::var("INFURA_ENDPOINT").expect("failed to get INFURA_ENDPOINT");
-    let res = eth::subscribe(&infura_endpoint).await;
-    println!("{:?}", res);
+
+    let users = Users::default();
+
+    tokio::task::spawn(eth::subscribe(infura_endpoint.to_string(), users.clone()));
+
+    let users = warp::any().map(move || users.clone());
 
     // POST /login
     let login = warp::path("login")
@@ -28,17 +43,10 @@ async fn main() {
     let ws = warp::path("ws")
         // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .map(|ws: warp::ws::Ws| {
+        .and(users)
+        .map(|ws: warp::ws::Ws, users| {
             // And then our closure will be called when it completes...
-            ws.on_upgrade(|websocket| {
-                // Just echo all messages back...
-                let (tx, rx) = websocket.split();
-                rx.forward(tx).map(|result| {
-                    if let Err(e) = result {
-                        eprintln!("websocket error: {:?}", e);
-                    }
-                })
-            })
+            ws.on_upgrade(move |socket| user_connected(socket, users))
         });
 
     // GET /healthz => 200 OK
@@ -47,4 +55,21 @@ async fn main() {
 
     let routes = healthz.or(api.with(warp::log("api")));
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+}
+
+async fn user_connected(ws: WebSocket, users: Users) {
+    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+
+    eprintln!("new user: {}", my_id);
+
+    let (user_ws_tx, _user_ws_rx) = ws.split();
+
+    let (tx, rx) = mpsc::unbounded_channel();
+    tokio::task::spawn(rx.forward(user_ws_tx).map(|result| {
+        if let Err(e) = result {
+            eprintln!("websocket send error: {}", e);
+        }
+    }));
+
+    users.write().await.insert(my_id, tx);
 }
